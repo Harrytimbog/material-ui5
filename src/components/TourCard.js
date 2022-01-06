d35accbe7392fd5035d1213770098f1bf74d7c12
Timilehin Arigbede <Harriemannie@gmail.com>
2022-01-06 15:23:13 +0100

    Add Icon, typography and Box from material UI to Tour card

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -2,6 +2,7 @@ import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { AccessTime } from "@mui/icons-material";
 
 const TourCard = () => {
   return (
@@ -16,6 +17,17 @@ const TourCard = () => {
           <Typography variant="subtitle1" component="h2">
             Immerse into the Falls
           </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center"
+            }}
+          >
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              5 hours
+            </Typography>
+          </Box>
         </Box>
       </Paper>
     </Grid>
