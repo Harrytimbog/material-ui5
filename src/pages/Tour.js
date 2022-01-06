51fb56982c4f74dcf0f20f82a99e1062c76b8c28
Timilehin Arigbede <Harriemannie@gmail.com>
2022-01-06 21:10:40 +0100

    Add more blab content and Accordion component from material UI

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import Container from "@mui/material/Container";
+import CustomizedAccordions from "../components/Accordion";
 import ImageCollage from "../components/ImageCollage";
 
 const Tour = () => (
@@ -8,8 +9,35 @@ const Tour = () => (
       Explore the World in Vegas
     </Typography>
     <Box marginTop={3} sx={{ display: "flex" }}>
-      <img src="https://images.unsplash.com/photo-1470076892663-af684e5e15af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1916&q=80" alt="vegas" height={325} width={600} />
+      <img
+        src="https://images.unsplash.com/photo-1470076892663-af684e5e15af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1916&q=80"
+        alt="vegas"
+        height={325}
+        width={600}
+      />
       <ImageCollage />
+    </Box>
+    <Box>
+      <Typography variant="h6" component="h4" marginTop={3}>
+        About this ticket
+      </Typography>
+      <Typography variant="paragraph" component="p" marginTop={3}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </Typography>
+    </Box>
+    <Box>
+      <Typography variant="h6" component="h4" marginTop={3} marginBottom={2}>
+        Frequently Asked Questions
+      </Typography>
+      <CustomizedAccordions />
     </Box>
   </Container>
 );
