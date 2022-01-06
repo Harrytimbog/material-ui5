fc9ea3a3699348b13fbc88d43b865267f796e007
Timilehin Arigbede <Harriemannie@gmail.com>
2022-01-06 18:26:19 +0100

    Replace static data with a prepared data.json and integrate details into Tour card and App components

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -31,61 +31,56 @@ const theme = createTheme({
   }
 })
 
-const TourCard = () => {
+const TourCard = ({tour}) => {
   return (
     <Grid item xs={3}>
-    <ThemeProvider theme={theme}>
-
-      <Paper elevation={3}>
-        <img
-          src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
-          alt=""
-          className="img"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Immerse into the Falls
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img src={tour.image} alt={tour.name} className="img" />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              {tour.name}
             </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.duration} hours
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+              marginTop={3}
+            >
+              <Rating
+                name="read-only"
+                value={tour.rating}
+                precision={0.5}
+                size="small"
+                readOnly
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.rating}
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
+                ({tour.numberOfReviews} reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                From C ${tour.price}
+              </Typography>
+            </Box>
           </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-            marginTop={3}
-          >
-            <Rating
-              name="read-only"
-              value={4.5}
-              precision={0.5}
-              size="small"
-              readOnly
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body3" component="p" marginLeft={1.5}>
-              (655 reviews)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="h3" marginTop={0}>
-              From C $147
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
-    </ThemeProvider>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
